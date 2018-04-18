4c87545817c682ad8bc5d79e7573a8cec7fc34aa
Stanley Huang <infamous_godhand@yahoo.com>
2018-04-18 12:32:02 +0000

    Fetch exercise with bitcoin coindesk api

---
+++ main.js
@@ -0,0 +1,15 @@
+/*global fetch*/
+
+function calculate(){
+    //alert("hi");
+    var bit_url="https://api.coindesk.com/v1/bpi/currentprice.json";
+    fetch(bit_url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(myJson=>{
+        console.log(myJson.bpi.USD.rate);
+        var value=document.getElementById("money");
+        value.innerHTML=myJson.bpi.USD.rate;
+    });
+}
